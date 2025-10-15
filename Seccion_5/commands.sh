8bf602b1eda60257aaf1c63afc1e5644286f566b
SRCAndy <andy.cano@utp.edu.co>
2025-10-15 01:04:20 -0500

    Laboratorio 1 terminado

--- Seccion_5/commands.sh
+++ Seccion_5/commands.sh
@@ -6,3 +6,22 @@ aws iam create-user --user-name test
 
 #Asignacion de permisos
 aws iam attach-user-policy --user-name test --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+
+#Resolucion de problema 1
+
+#Creacion de usuarios
+aws iam create-user --user-name Usuario1
+aws iam create-user --user-name Usuario2
+aws iam create-user --user-name Usuario3
+
+#Asignacion de permisos
+aws iam attach-user-policy --user-name Usuario1 --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
+aws iam attach-user-policy --user-name Usuario2 --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
+aws iam attach-user-policy --user-name Usuario3 --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
+
+#Verificacion de permisos
+aws iam list-attached-user-policies --user-name Usuario1
+aws iam list-attached-user-policies --user-name Usuario2
+aws iam list-attached-user-policies --user-name Usuario3
+
+
